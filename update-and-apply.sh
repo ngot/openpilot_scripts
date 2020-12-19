e939b9b52294257ae9912c3b699d4347c7d698b4
ngot <zhuanghengfei@gmail.com>
2020-12-19 21:59:04 +1100

    init

---
+++ update-and-apply.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+./update-openpolit.sh
+
+git apply my.patch
---
+++ update-openpolit.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cd /data
+rm -rf openpilot
+git clone -b master-ci https://github.com/commaai/openpilot
+reboot
